1a42303f143a5408301d8fa0290ec2a5e6d197ea
Soumodeep-Das <146613886+Soumodeep-Das@users.noreply.github.com>
2024-11-04 21:39:32 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider"
 import { useMediaQuery } from "@/hooks/useMediaQuery"
 import { createUser, getUnreadNotifications, markNotificationAsRead, getUserByEmail, getUserBalance } from "@/utils/db/actions"
 
-const clientId = "BJKdDFkNtkWX87XqkuWrDu4rbkSvWyQZ5lswS0ucINxxcN0inRVW8zzKAywPPzgiOHP7_3PcfFwfpvcQvSdaLRs";
+const clientId = "BDIm7MhUm2PPE9KUQJ-gZ08A6ZI3-BFLzZYGi9ieC_mng0Ugu_ELX4gPzMlFRxv7JgzNSPAjRm_dj1BMVlFxavw";
 
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
